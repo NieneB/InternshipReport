4fed06c68f7cc5e51950250c025ed8f24da4494a
NieneB <niene.boeijen@gmail.com>
2015-07-28 13:42:42 +0200

    title page toc

--- js/toc.js
+++ js/toc.js
@@ -2,7 +2,7 @@ var hoofdstukken =  d3.selectAll("h1, h2, h3")
 console.log(hoofdstukken)
 
 
-var tov = d3.select("#toc").append("ol")
+var tov = d3.select("#toc").append("ul").attr("class", "toc")
 
 tov.selectAll("li")
   .data(hoofdstukken[0])
