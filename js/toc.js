8970c349af840683a061f298a141793a2804c9f8
NieneB <niene.boeijen@gmail.com>
2015-07-28 18:08:44 +0200

    toc style almost there

--- js/toc.js
+++ js/toc.js
@@ -5,11 +5,14 @@ console.log(hoofdstukken)
 var tov = d3.select("#toc").append("ul").attr("class", "toc")
 
 tov.selectAll("li")
-  .data(hoofdstukken[0])
+  .data(hoofdstukken[0].classname != "nocount")
   .enter()
   .append("li")
   .attr("class", function(d){
     return "toc-"+ d.localName
+  })
+  .attr("href", function(d){
+    return d.id
   })
   .text(function(d){
      return d.innerHTML });
