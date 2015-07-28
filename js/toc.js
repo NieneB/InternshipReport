a7e76f66a19a9c14937409f5a33996300ebe20f5
Bert Spaan <bert.spaan@gmail.com>
2015-07-28 18:50:46 +0200

    ul li ul li ul li ul li

--- js/toc.js
+++ js/toc.js
@@ -1,12 +1,68 @@
-var hoofdstukken =  d3.selectAll("h1, h2, h3")
-console.log(hoofdstukken)
+var MAX_LEVEL = 3;
+
+function createHeadingTree() {
+  var tree = {
+    element: document.body,
+    children: []
+  };
+
+  var allHeadingsSelector = Array.apply(null, new Array(MAX_LEVEL)).map(function(d, i) {
+    return 'h' + (i + 1) + ':not(.nocount)';
+  }).join(', ');
+
+  var previousLevel = 0;
+  var currentParent = tree;
+  [].forEach.call(document.querySelectorAll(allHeadingsSelector), function(heading) {
+    var level = parseInt(heading.tagName[1]);
+
+    var treeItem = {
+      heading: heading,
+      children: []
+    };
+
+    if (level == previousLevel) {
+      currentParent.children.push(treeItem);
+    } else if (level > previousLevel) {
+      for (var i = 0; i < level - (previousLevel + 1); i++) {
+        var emptyItem = {
+          heading: null,
+          children: []
+        };
+        currentParent.children.push(emptyItem);
+        currentParent = emptyItem;
+      }
+      currentParent.children.push(treeItem);
+      currentParent = treeItem;
+    } else if (level < previousLevel) {
+      currentParent = tree;
+      for (var i = 0; i < level - 1; i++) {
+        currentParent = currentParent.children[currentParent.children.length - 1];
+      }
+      currentParent.children.push(treeItem);
+    }
+    previousLevel = level;
+  });
+
+  return tree.children;
+}
+
+function createToc(container, headings) {
+  var item = d3.select(container).append('ol').selectAll('li')
+      .data(headings)
+    .enter().append('li');
+
+  item.append('a')
+      .attr('href', function(d) {
+        return '#' + d.heading.id;
+      })
+      .html(function(d) {
+        return d.heading.innerHTML;
+      })
+      .each(function(d) {
+        createToc(this, d.children);
+      });
+}
 
 
-var tov = d3.select("#toc").append("ul").attr("class", "toc")
-
-tov.selectAll("li")
-  .data(hoofdstukken[0])
-  .enter()
-  .append("li")
-  .text(function(d){
-     return d.innerHTML});
+var tree = createHeadingTree();
+createToc('#toc', tree)
